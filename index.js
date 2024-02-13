5c3c7d288f728a8b0fe32922151dc291411f25f9
vish <11762008+00000vish@users.noreply.github.com>
2024-02-12 23:22:08 -0600

    add canvas

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-function start() {
-  var div = document.getElementById('helloWorld');
-  div.innerText = "hello world";
+function setCanvasImage(imageFile) {
+  var canvas = document.getElementById("imageDisplay");
+  var ctx = canvas.getContext("2d");
+
+  var image = new Image();
+  image.onload = function() {
+    ctx.drawImage(image, 0, 0);
+    console.log("print done ")
+  };
+
+  image.src = imageFile;
 }
 
-start();
+function readFile(input) {
+
+  let file = input.files[0];
+
+  let reader = new FileReader();
+
+  reader.readAsDataURL(file);
+
+  reader.onload = function() {
+    setCanvasImage(reader.result);
+  };
+
+  reader.onerror = function() {
+    console.log(reader.error);
+  };
+}
